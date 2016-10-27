c6a24e20202c74f893c687683045e77420238bc5
osawa-jun <osawa-jun@KYCNT1610-06.local>
2016-10-27 19:32:45 +0900

    p.122

--- topic04/hashReference.pl
+++ topic04/hashReference.pl
@@ -20,11 +20,33 @@
 $refMonth = \%month;
 print "ref to month hash: $refMonth \n";
 
+# 上記は次のようにもかける
+
+$refMonth = {
+    Jan  => 1 , 
+    Feb  => 2 , 
+    Mar  => 3 , 
+    Apr  => 4 , 
+    May  => 5 , 
+    Jun  => 6 , 
+    Jul  => 7 , 
+    Aug  => 8 , 
+    Sep  => 9 , 
+    Oct  => 10, 
+    Nov  => 11, 
+    Dec  => 12, 
+};
+
 @keys = keys %$refMonth;
-print "keys: @keys \n";
+print "ばらばらなkeys: @keys \n";
 
 @sorted_key = sort keys %$refMonth;
 print "sorted key: @sorted_key \n";
 
 print "Jan is Month No.$$refMonth{Jan} \n";
 print "Feb is Month No.$refMonth->{Feb} \n";
+
+# ハッシュスライス
+for $mon (@{$refMonth}{Sep, Oct, Nov}){
+    print "Autumn contains month No. $mon \n";
+}
