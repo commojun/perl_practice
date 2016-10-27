b47309ec7bdb05303c4984f68b9501c7515f8768
osawa-jun <osawa-jun@KYCNT1610-06.local>
2016-10-27 19:12:36 +0900

    p.119

---
+++ topic04/hashReference.pl
@@ -0,0 +1,30 @@
+#
+# hashReference.pl
+# ハッシュ変数のリファレンス作成とアクセスの練習
+
+%month = (
+    Jan  => 1 , 
+    Feb  => 2 , 
+    Mar  => 3 , 
+    Apr  => 4 , 
+    May  => 5 , 
+    Jun  => 6 , 
+    Jul  => 7 , 
+    Aug  => 8 , 
+    Sep  => 9 , 
+    Oct  => 10, 
+    Nov  => 11, 
+    Dec  => 12, 
+    );
+
+$refMonth = \%month;
+print "ref to month hash: $refMonth \n";
+
+@keys = keys %$refMonth;
+print "keys: @keys \n";
+
+@sorted_key = sort keys %$refMonth;
+print "sorted key: @sorted_key \n";
+
+print "Jan is Month No.$$refMonth{Jan} \n";
+print "Feb is Month No.$refMonth->{Feb} \n";
